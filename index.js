60905ed81f52646cb9211c9072220ff1ed042d71
Zakaria Ridouh <zridouh@live.com>
2016-08-20 09:34:56 -0700

    start work on new functions

--- index.js
+++ index.js
@@ -3,5 +3,27 @@
 let uniqueRandomArray = require('unique-random-array');
 let babyNames = require('./baby-names.json');
 
-exports.all = babyNames;
-exports.random = uniqueRandomArray(babyNames);
+module.exports= {
+  all: babyNames,
+  random: singleRandom,
+  randomAmount: randomAmount,
+  startsWith: startsWith
+}
+
+const singleRandom = () => uniqueRandomArray(babyNames)
+
+function randomAmount(number) {
+  if (typeof number === "number") {
+
+  } else {
+    return `Error parsing ${number}!`
+  }
+};
+
+function startsWith(letter) {
+  if ((letter.length >= 1) && (typeof letter === "string")) {
+
+  } else {
+    return `Error parsing ${letter}!`
+  }
+};
