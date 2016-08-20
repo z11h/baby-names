f4c8fee4ed2f4da2425828c8e815a06fc61afeb6
Amila Welihinda <amilajack@users.noreply.github.com>
2016-08-20 16:48:08 -0700

    Conformed CS

--- index.js
+++ index.js
@@ -3,27 +3,27 @@
 let uniqueRandomArray = require('unique-random-array');
 let babyNames = require('./baby-names.json');
 
-module.exports= {
+module.exports = {
   all: babyNames,
   random: singleRandom,
   randomAmount: randomAmount,
   startsWith: startsWith
-}
+};
 
-const singleRandom = () => uniqueRandomArray(babyNames)
+const singleRandom = () => uniqueRandomArray(babyNames);
 
 function randomAmount(number) {
   if (typeof number === "number") {
-
+    // @todo
   } else {
-    return `Error parsing ${number}!`
+    return `Error parsing ${number}!`;
   }
 };
 
 function startsWith(letter) {
   if ((letter.length >= 1) && (typeof letter === "string")) {
-
+    // @todo
   } else {
-    return `Error parsing ${letter}!`
+    return `Error parsing ${letter}!`;
   }
 };
